b592e0713703103ab91d13869c9d44c605492280
Godwin Idemudia <godwinide@gmail.com>
2023-11-03 14:01:44 +0100

    new

--- api/index.js
+++ api/index.js
@@ -18,7 +18,7 @@ router.get("/get-tables", async (req, res) => {
 
 router.get("/get-entry", async (req, res) => {
     try {
-        const entry = await Entry.findOne({ tableName: "Auto-Roulette" }, { sent: false });
+        const entry = await Entry.findOne({ tableName: "Auto-Roulette", sent: false },);
         const _entry = { ...entry };
         if (entry) {
             await Entry.updateMany({ tableName: "Auto-Roulette" }, { sent: true })
